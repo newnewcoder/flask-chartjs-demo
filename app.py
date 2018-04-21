a8ff60c9143c9fc92b04cc575322383d33e8570e
Pinky <pinkycho1221@gmail.com>
2018-04-21 16:52:21 +0800

    Update for incremental data

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import render_template, Flask, send_from_directory
+from flask import render_template, Flask, jsonify
 import random
 
 app = Flask(__name__)
@@ -11,6 +11,12 @@ def index():
     data = [random.randint(-100, 100) for x in labels]
 
     return render_template('index.html', data={'title': title, 'data': data, 'labels': labels} )
+
+@app.route('/get_new_data/')
+def get_new_data():
+    # simulate incremental data for ajax call...
+    nwe_data = {'value': random.randint(-100, 100)}
+    return jsonify(nwe_data)
 
 if __name__ == '__main__':
     app.run()
